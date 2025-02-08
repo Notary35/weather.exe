6cc4c42a5ca0d0cef691c6adeb863d00e0891307
Sergey Papilov <papilov.serhz@gmail.com>
2025-02-08 21:57:06 +0300

    pip innstall plyer requests

---
+++ weather.py
@@ -0,0 +1,11 @@
+import requests
+from winotify import Notification as WinNotification
+from requests.exceptions import RequestException
+from json.decoder import JSONDecodeError
+
+CITY = "Лиссабон"
+API_KEY = "23496c2a58b99648af590ee8a29c5348"
+UNITS = "metric"
+LANGUAGE = "ru"
+
+
